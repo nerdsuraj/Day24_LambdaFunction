76dbc80cea0a9f3cfcbbd81f4cd9d66a65842060
suraj <soorajkrpandit@gmail.com>
2022-05-28 11:37:45 +0530

    anonymous function are used

--- Day23_LambdaFunction/Program.cs
+++ Day23_LambdaFunction/Program.cs
@@ -11,6 +11,21 @@ namespace Day23_LambdaFunction
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to address book program..");
+            Console.WriteLine();
+
+            MakingFromPattern patterns = new MakingFromPattern();
+            Console.WriteLine(patterns.validateFirstName("Suraj"));
+            Console.WriteLine(patterns.validateLastName("kumar"));
+            Console.WriteLine(patterns.validateEmail("Soorajkrpandit@gmail.com"));
+            Console.WriteLine(patterns.validatePhoneNo("91 6200749278"));
+            Console.WriteLine(patterns.validatePassword("LabBridge@001"));
+            Console.WriteLine();
+
+            Console.WriteLine("To validate the user entry using lambda function");
+            List<Person> listed = new List<Person>();
+            LambdaAnynomous.ValidateUserEntry(listed);
+
+            Console.ReadKey();
 
         }
     }
